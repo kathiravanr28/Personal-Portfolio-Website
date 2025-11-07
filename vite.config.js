ea15d17692256c40a5008cfe4632c8895c1a10dc
Kathir <kathiravanr280303@gmail.com>
2025-11-07 18:21:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Personal-Portfolio-Website/', // 👈 this must match your repo name
   plugins: [react()],
 })
+
