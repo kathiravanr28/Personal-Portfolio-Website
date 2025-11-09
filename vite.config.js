3546a6eadeb019b3d04321a9c359c18fec45e966
Kathir <kathiravanr280303@gmail.com>
2025-11-09 19:26:18 +0530

    chore : set base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Personal-Portfolio-Website/', // 👈 this must match your repo name
+  base: '/Personal-Portfolio-Website/',
   plugins: [react()],
 })
-
